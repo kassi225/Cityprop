cc57f56ebb05ed9576ec93b7c8027d14f5d106c3
Gerard Kassi <kassigerard28@gmail.com>
2025-12-15 02:06:13 +0000

    Add build.sh for Render deployment

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# build.sh : script pour installer les dépendances
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Applying database migrations..."
+python manage.py migrate
