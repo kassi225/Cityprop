d30f650bd4c3e32ffad1bd937914b0198675edc5
Gerard Kassi <kassigerard28@gmail.com>
2025-12-15 02:43:55 +0000

    Remove Teletravail references

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Teletravail.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestion.settings')
 
 application = get_wsgi_application()
